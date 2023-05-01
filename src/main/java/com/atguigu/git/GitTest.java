bb20161d05e02502c27e794d415985827c74af13
shark <shark@qq.com>
2023-05-01 16:11:30 +0800

    third idea  commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是第一个 git file");
         System.out.println("这是第一个 git file2");
+        System.out.println("这是第一个 git file3");
     }
 }
